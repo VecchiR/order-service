1e5f55c1d4acf5dddde038cae6f3493148ecc41e
Vecchi <79600745+VecchiR@users.noreply.github.com>
2025-03-24 16:50:10 -0300

    Setup Java project

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        Product testProduct = new Product("Nice product 1", 1.2);
+        System.out.println(testProduct);
+    }
+}
